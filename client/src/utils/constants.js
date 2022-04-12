b3334e81e0ca18c4b9e9261637fa6d5d8d224ff6
kmcodes <kmcodesdev@gmail.com>
2022-04-12 05:00:30 +0200

    first commit

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi;
+export const contractAddress = '0x4f95887c55af5c933bcf69f80a1a9b5983ecd264'
---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,11 @@
+require('@nomiclabs/hardhat-waffle');
+
+module.exports = {
+  solidity: '0.8.0',
+  networks: {
+    ropsten: {
+      url: 'https://eth-ropsten.alchemyapi.io/v2/Dm5PrKF40uE5VBI6WZllldEprRSR1eHf',
+      accounts: ['821004734c43bf5eccf8ba4c00304f53e9ebaaefa422bbf2224c411f1bcbfc72'],
+    }
+  }
+}
